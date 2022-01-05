a18660436a2ae0307a206ff8a7cee7ffe9c3c79d
SEUNGHAN BAEK <84169773+seunghan-baek@users.noreply.github.com>
2022-01-05 22:45:22 +0900

    CRUD 기능 추가 전 테스트Unit 정상 작동확인

---
+++ src/main/java/com/seung/booboo/springboot/domain/posts/PostsRepository.java
@@ -0,0 +1,1 @@
+package com.seung.booboo.springboot.domain.posts;import org.springframework.data.jpa.repository.JpaRepository;public interface PostsRepository extends JpaRepository<Posts, Long> {                                //JpaRepository<Entity 클래스, PK 타입>}
