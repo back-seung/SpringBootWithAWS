4f7c02325f3cdaf08dccb37077e8b5ac9d40eb55
megaseunghan <skud113@naver.com>
2022-01-07 16:35:38 +0900

    Spring Security init

---
+++ src/main/java/com/seung/booboo/springboot/domain/user/Role.java
@@ -0,0 +1,1 @@
+package com.seung.booboo.springboot.domain.user;import lombok.Getter;import lombok.RequiredArgsConstructor;@Getter@RequiredArgsConstructorpublic enum Role {    GUEST("ROLE_GUEST", "손님"),    USER("ROLE_USER", "일반 사용자");    // spring security에서는 권한 코드에 ROLE_이 앞에 있어야만 한다.    private final String key;    private final String title;}
---
+++ src/main/java/com/seung/booboo/springboot/domain/user/UserRepository.java
@@ -0,0 +1,1 @@
+package com.seung.booboo.springboot.domain.user;import org.springframework.data.jpa.repository.JpaRepository;import java.util.Optional;public interface UserRepository extends JpaRepository<User, Long> {    Optional<User> findByEmail(String email);}
