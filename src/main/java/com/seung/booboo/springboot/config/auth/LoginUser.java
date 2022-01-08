9bd5c9e4375513b7445a3ea0731a68223128e2dc
SEUNGHAN BAEK <84169773+seunghan-baek@users.noreply.github.com>
2022-01-09 02:14:56 +0900

    @LoginUser 어노테이션 추가

---
+++ src/main/java/com/seung/booboo/springboot/config/auth/LoginUser.java
@@ -0,0 +1,1 @@
+package com.seung.booboo.springboot.config.auth;import java.lang.annotation.ElementType;import java.lang.annotation.Retention;import java.lang.annotation.RetentionPolicy;import java.lang.annotation.Target;@Target(ElementType.PARAMETER) // 어노테이션 생성될 수 있는 위치@Retention(RetentionPolicy.RUNTIME)public @interface LoginUser { // 어노테이션 클래스 지정.}
