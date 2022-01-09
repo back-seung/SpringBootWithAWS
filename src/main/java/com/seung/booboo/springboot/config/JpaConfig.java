ce95a4be3005e806334a70917fc75c8d7ee8dc67
megaseunghan <skud113@naver.com>
2022-01-09 22:04:42 +0900

    test Code MOD for Spring Security

---
+++ src/main/java/com/seung/booboo/springboot/config/JpaConfig.java
@@ -0,0 +1,1 @@
+package com.seung.booboo.springboot.config;import org.springframework.context.annotation.Configuration;import org.springframework.data.jpa.repository.config.EnableJpaAuditing;@Configuration@EnableJpaAuditing // JPA Auditing 활성화public class JpaConfig {}
